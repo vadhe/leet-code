f98770d1960f2c953c6ef25acdbe79fe6f54583c
Rivaldi Putra <36479850+vadhe@users.noreply.github.com>
2023-07-22 19:27:28 +0800

    Time: 76 ms (10.99%), Space: 42.1 MB (76.55%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  
+  if (strs.length === 0) {
+    return '';
+  }
+  
+  let commonPrefix = strs[0];
+  
+  for (let i = 1; i < strs.length; i++) {
+    let j = 0;
+    
+    while (j < commonPrefix.length && j < strs[i].length && commonPrefix[j] === strs[i][j]) {
+      j++;
+    }
+    
+    commonPrefix = commonPrefix.slice(0, j);
+    
+    if (commonPrefix === '') {
+      break;
+    }
+  }
+  
+  return commonPrefix;
+
+
+};
