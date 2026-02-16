3857142d1864809aaa801081f42e9a9909b7ca82
Rivaldi Putra <36479850+vadhe@users.noreply.github.com>
2026-02-16 09:18:29 +0800

    Time: 16 ms (74.02%), Space: 74.4 MB (43.33%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,28 +3,13 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-//     let seen = new Set()
-//     // for(let i=0; i< nums.length; i++){
-//     //     console.log("ok",seen)
-//     //     if(seen.has(nums[i])){
-//     //         return true
-//     //     }
-//     //     seen.add(nums[i], nums[i]);
-//     // }
-//     for (const num of nums) {
-//      if (seen.has(num)) {
-//        return true;
-//      }
-//      seen.add(num);
-//    }
-//     return false
-const seen = new Set();
-
-for (let i = 0; i < nums.length; i++) {
-    if (seen.has(nums[i])) return true;
-    seen.add(nums[i]);
-}
-return false;
-
+    let seen = new Set()
+    for(let i=0; i< nums.length; i++){
+        if(seen.has(nums[i])){
+            return true
+        }
+        seen.add(nums[i]);
+    }
+    return false
 };
 
