e96b10dabda1aa44359b5bb0910470c16481f73d
Rivaldi Putra <36479850+vadhe@users.noreply.github.com>
2026-02-18 16:55:50 +0800

    Time: 2 ms (72.73%), Space: 58.2 MB (29.89%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+//     let hash = new Map();
+//      for(const v of nums){
+//         if(hash.has(v)) {
+//            hash.set(v, hash.get(v) +1)
+//         } else {
+//             hash.set(v, 1);
+//         }
+//      }
+//      let maxKey = null;
+// let maxValue = -Infinity;
+
+// for (let [key, value] of hash) {
+//     if (value > maxValue) {
+//         maxValue = value;
+//         maxKey = key;
+//     }
+// }
+// return maxKey
+let count = 0;
+let candidate
+for(const v of nums) {
+    if(candidate === undefined) {
+        candidate = v;
+    } else if (candidate === v) {
+        count++
+    } else if(count === 0) {
+            candidate = v
+    }else {
+        count--
+      
+    }
+}
+
+return candidate
+};
