8976a2bee5a7eda6735f2447492542162327fe85
Rivaldi Putra <36479850+vadhe@users.noreply.github.com>
2026-02-20 14:03:33 +0800

    Time: 0 ms (100%), Space: 55.2 MB (77.91%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -4,22 +4,21 @@
  */
 var singleNumber = function(nums) {
 
-    let candidate = new Map()
-    for(let i =0;  i< nums.length; i++) {
-        if(candidate.get(nums[i])) {
-            candidate.set(nums[i], candidate.get(nums[i]) + 1)
-        } else {
-             candidate.set(nums[i],1)
-        }
-    }
+//     let candidate = new Map()
+//     for(let i =0;  i< nums.length; i++) {
+//         if(candidate.get(nums[i])) {
+//             candidate.set(nums[i], candidate.get(nums[i]) + 1)
+//         } else {
+//              candidate.set(nums[i],1)
+//         }
+//     }
+return nums.reduce((acc, n) => acc ^ n, 0);
 
 
-
-
-for (let [key, value] of candidate) {
-    if(value === 1) {
-        return key
-    }
-}
-    return 0
+// for (let [key, value] of candidate) {
+//     if(value === 1) {
+//         return key
+//     }
+// }
+//     return 0
 };
