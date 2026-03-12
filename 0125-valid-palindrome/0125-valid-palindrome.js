abade9c18e1b769c063b36c6792b5efcb702628e
Rivaldi Putra <36479850+vadhe@users.noreply.github.com>
2026-03-13 03:35:48 +0800

    Time: 134 ms (5.03%), Space: 64.4 MB (6.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,26 +3,19 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let l = 0
-    let r = s.length - 1
-
-    while (l < r) {
-        while (l < r && !isAlphaNum(s[l])) l++
-        while (l < r && !isAlphaNum(s[r])) r--
-
-        if (s[l].toLowerCase() !== s[r].toLowerCase()) return false
-
-        l++
-        r--
-    }
-    return true
-}
-
-function isAlphaNum(c) {
-    let code = c.charCodeAt(0)
-    return (
-        (code >= 48 && code <= 57) ||
-        (code >= 65 && code <= 90) ||
-        (code >= 97 && code <= 122)
-    )
+   const string = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+//    if(string.match(/[^a-zA-Z]/g)){
+//     return false
+//    }
+   let L = 0
+   let R = string.length -1
+   while(L < R) {
+    console.log(`${string[L].toLowerCase()} ---- ${string[R]} -------${string[L] !== string[R]}`)
+     if(string[L] !== string[R]){
+        return false
+     }
+     L++
+     R--
+   }
+   return true
 }
